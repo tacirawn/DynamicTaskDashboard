90565891cbe8825e3ab8573114dbc913bc0617d6
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-23 20:15:29 +0300

    commit

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+import { createElement } from "react";
 import fetchTasks from "./api";
 import Task from './taskManager';
 import TaskManager from './taskManager';
 const loadTasksButton = document.getElementById(loadTasksBtn);
+
+taskManager = new TaskManager();
 
 addEventListener('click', async() => {
     console.log("Loading tasks...");
@@ -11,7 +14,27 @@ addEventListener('click', async() => {
         console.log(rawData);
         string = JSON.stringify(rawData);
         parsed = JSON.parse(string);
+        const taskObjects = parsed.map(item => {
+            return new Task(item.id, item.title, item.completed);
+        });
+        taskManager.setTasks(taskObjects);
+        render();
     }catch(error){
         statusMessage.textContent = "Error occured.";
     }
 });
+
+function render(){
+    const div = document.getElementById("taskList");
+    div.innerHTML("");
+    const tasks = taskManager.tasks;
+    tasks.forEach(element => {
+        const box = createElement("div");
+        const span = createElement("span");
+        const toggleBtn = createElement("button");
+        const deleteBtn = createElement("button");
+    });
+    const button = createElement("button");
+}
+
+
