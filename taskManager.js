2076b40e5414ad897d9a88d2d8741ac02dca48a1
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-23 18:26:33 +0300

    commit

--- taskManager.js
+++ taskManager.js
@@ -1,1 +1,51 @@
-taskManager
+class Task{
+    constructor(id, title, completed){
+        Object.defineProperty(this, "id", {
+            value: id,
+            writable: false,
+            configurable: false
+        });
+        this.title = title;
+        this.completed = completed;
+    }
+
+    toggle(){
+        if(this.completed){
+            newCompleted = false;
+        }
+        else {
+            newCompleted = true;
+        }
+        newTask = new Task(this.id, this.title, newCompleted);
+        return newTask;
+    }
+}
+
+
+class TaskManager{
+    tasks = [];
+
+    setTasks(tasks){
+        this.tasks = tasks;
+    }
+
+    addTask(task){
+        taskList= this.tasks;
+        taskList.push(task);
+        this.tasks = taskList;
+    }
+
+    removeTask(taskId){
+        taskList = [];
+        for(let t of this.tasks){
+            if(t.id != taskId){
+                taskList.push(t);
+            }
+        }
+        this.tasks = taskList;
+    }
+
+    toggleTask(taskId){
+
+    }
+}
