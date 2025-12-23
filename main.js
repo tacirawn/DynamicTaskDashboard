6a50ce5e5e992aeeb99ce7c1a5f1ae24e60cdcb1
Nisa Ayaz <nnisayazz@gmail.com>
2025-12-23 19:44:19 +0300

    commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import fetchTasks from "./api";
-
+import Task from './taskManager';
+import TaskManager from './taskManager';
 const loadTasksButton = document.getElementById(loadTasksBtn);
 
 addEventListener('click', async() => {
@@ -8,7 +9,8 @@ addEventListener('click', async() => {
     try{
         const rawData = await fetchTasks();
         console.log(rawData);
-        
+        string = JSON.stringify(rawData);
+        parsed = JSON.parse(string);
     }catch(error){
         statusMessage.textContent = "Error occured.";
     }
